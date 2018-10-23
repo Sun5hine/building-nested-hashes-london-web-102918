a600679f1878dcd064aec3c718d32a52ce283858
sun5hine <af5kh@yahoo.com>
2018-10-23 14:26:03 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach => {}
-      :matriach => {}
-      :hero => {}
+      :patriach => {},
+      :matriach => {},
+      :hero => {},
       :hero_friends =>[]
       }, 
     :capulet => {
-      
-      }}
+       :patriach => {},
+      :matriach => {},
+      :hero => {},
+      :hero_friends =>[]
+      }
+    
+  }
 
   
 end
